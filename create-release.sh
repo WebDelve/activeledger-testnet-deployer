b973dd711d15b4d79f487ac1b246baa6500b6b30
Chris <4070267+Chris-Holdt@users.noreply.github.com>
2023-07-27 19:09:51 +0100

    Moved example files to examples folder and updated create-release.sh to reflect

--- create-release.sh
+++ create-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 go build -o deployer
-tar -czvf latest-linux.tar.gz deployer contract-manifest-example.json setup-output-example.json config-example.json
+tar -czvf latest-linux.tar.gz deployer examples
